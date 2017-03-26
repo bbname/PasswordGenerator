70c46886c27e2d2a8626f971e39d224f9536cac8
bbname <bartlomiej.bienczyk@gmail.com>
2017-03-26 16:27:01 +0200

    Added some new classes to transform Signs - Chars into ints and to set indexes.

---
+++ BussinesLogic/BuilderPasswordGeneratorTransformSigns.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BussinesLogic.Interfaces;
+using
+
+namespace BussinesLogic
+{
+    public class BuilderPasswordGeneratorTransformSigns : IBuilderPasswordGeneratorTransformSigns
+    {
+        private bool[] _signTypes;
+        public BuilderPasswordGeneratorTransformSigns(bool[] singTypes)
+        {
+            this._signTypes = _signTypes;
+        }
+
+
+        public int[] GetSignsIndexes()
+        {
+            
+        }
+    }
+}
---
+++ DataServices/Data/SignIdexes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataServices.Data
+{
+    public static class SignIdexes
+    {
+        public static int [] SmallLetters { get { return null; } }
+
+        public static int [] SetLettersIndexes()
+        {
+            int smallLetters = Signs.Letters.Length;
+            int capitalLetters = Signs.Letters.Length;
+            int smallPolishLetters = Signs.PolishLetters.Length;
+            int capitalPolishLetters = Signs.PolishLetters.Length;
+            int specialSigns = Signs.SpecialSigns.Length;
+            int totalSignsNumber = smallLetters + capitalLetters + smallPolishLetters + capitalPolishLetters + specialSigns;
+
+            bool[] signTypes = { smallLetters > 0, capitalLetters > 0,
+                                 smallPolishLetters > 0, capitalPolishLetters > 0, specialSigns > 0  };
+            int[,] lettersIndexes = new int[totalSignsNumber, signTypes.Length];
+
+            int lettersNumber = smallLetters;
+            while ()
+            {
+                for (int i = 0; i < totalSignsNumber; i++)
+                {
+                    lettersIndexes[i, ] = i;
+                }
+            }
+
+            return lettersIndexes;
+        }
+    }
+}
